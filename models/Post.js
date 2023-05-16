f1241ee7ca0eaf19a78f209dc2856442b853ba34
Fatima Basharat <fatima8746@gmail.com>
2023-05-16 15:29:49 -0400

    initial commit

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Array,
+    required: true,
+  },
+  bookmarks: {
+    type: Array,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
